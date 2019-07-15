0e668564cc28808039c13701f8ca0e7b7447922b
14808 <14808@192.168.0.30>
2019-07-14 19:51:12 -0600

    Started project classes

--- Weather Wiki/src/main.java
+++ Weather Wiki/src/main.java
@@ -1,18 +1,18 @@
 /**
- * 
- */
-
-/**
- * @author 14808
+ * Weather Wiki
+ * Code by Bradley Trede & William Nathanial Markham Brown
  *
  */
 public class main {
 
-	/**
-	 * 
-	 */
-	public main() {
-		// TODO Auto-generated constructor stub
-	}
+    public static void main(String[] args) {
+        
+        Menu menu = new weatherMainMenu();
+        
+        menu.display();
+        
+        System.out.println("Thank you for visiting the Weather Wiki");
+
+    }
 
 }
---
+++ Weather Wiki/src/weatherMainMenu.java
@@ -0,0 +1,78 @@
+import java.awt.MenuItem;
+import java.awt.Menu;
+
+public class weatherMainMenu extends main {
+
+	public weatherMainMenu() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+    @Override
+    protected String getTitle() {
+        return "Weather Wiki Main Menu";
+    }
+    
+
+    @Override
+    protected String getDescription() {
+        return "Welcome to the Weather Wiki.  Your source for researching historical weather"
+        		+ "conditions by type and continent. Please select your entry to begin:";
+    }
+    
+    /**
+     * Provide the list of menu items.
+     * @return
+     */
+    @Override
+    protected MenuItem[] getMenuItems() {
+        return new MenuItem[] {
+                new MenuItem('1', "Choose Continent"),
+                new MenuItem('2', "Import Data into the Wiki"),
+                new MenuItem('3', "Export Data to file from Wiki"),
+                new MenuItem('H', "Help Menu"),
+                new MenuItem("Quit Program")
+              
+        };
+    }
+    
+    
+    /**
+     * Handle the user's menu selection.
+     * @param key
+     * @return false if the menu should exit and return to whatever called it.
+     */
+    @Override
+    protected boolean handleMenuSelection(char key) {
+        
+        
+        
+        switch (Character.toUpperCase(key)) {
+            case '1': 
+                System.out.println("");
+                break;
+            case '2':
+                System.out.println("You typed 2");
+                break;
+            case '3':
+                System.out.println("You typed 3");
+                break;
+                
+            case 'H':
+                Menu helpMenu = new HelpMenu();
+                helpMenu.display();
+                break;
+                
+            case 'Q':
+                return false;
+                
+            default:
+                System.out.println("Invalid Entry");
+            
+        }
+        
+        return true;
+    }
+}
+    
